6ed32b1c2a458cf69b5a43f8711996e0311061ef
Oscar <oscargarcia.costa@telefonica.com>
2020-11-24 10:56:34 +0000

    First analysis of the DF

--- wikipedia_scrapping.py
+++ wikipedia_scrapping.py
@@ -19,7 +19,61 @@ import wikipedia_scrapping_functions as wsf
 
 
 
+import pandas as pd
+# from wikipedia_scrapping_parameters import wiki_cities_100k_url, csvs_folder
+
+
+
 
 cities_climate = wsf.import_and_reformat_climate_table()
 
 
+# cities_climate['rnk_asc'] = cities_climate.groupby(['City', 'Measure'])['Value'].rank(method = 'first', ascending=True)
+
+
+cities_climate['rnk_asc'] = cities_climate.loc[cities_climate['Month'] != 'Annual']\
+    .groupby(['City', 'Measure'])[['Value', 'Month_id']].rank(method = 'first', ascending=True)
+cities_climate['rnk_desc'] = cities_climate.loc[cities_climate['Month'] != 'Annual']\
+    .groupby(['City', 'Measure'])[['Value', 'Month_id']].rank(method = 'first', ascending=False)
+
+
+cities_climate.loc[cities_climate['rnk_desc']<= 3]\
+    .groupby(['City', 'Measure'])['Value'].mean()
+    
+
+
+
+conditions = {
+    'Sun hours' : ['Annual', 'Mean monthly sunshine hours', [1800, 3000]],
+    'Max summer' : ['Top 3', 'Average high (°C)', [22, 28]],
+    'Min summer' : ['Bottom 3', 'Average low (°C)', [-2, 6]]
+    
+    }
+
+import statistics
+l = [22, 29]
+statistics.mean(l)
+
+
+for key, value in conditions.items():
+    print(key)
+    print(value)
+    
+a = ((cities_climate['Month'] == 'Annual') &
+     (cities_climate['Measure'] == 'Mean monthly sunshine hours') &
+     (cities_climate['Value'] >= 1800) &
+     (cities_climate['Value'] <= 3000) )
+                   
+b = ((cities_climate['Month'] == 'Annual') &
+     (cities_climate['Measure'] == 'Mean monthly sunshine hours') &
+     (cities_climate['Value'] >= 1800) &
+     (cities_climate['Value'] <= 3000) )
+
+
+statistics.mean(conditions['Sun hours'][2])
+                   
+selected_cities = cities_climate.loc[a].copy()
+selected_cities['Dif'] = selected_cities['Value'] - statistics.mean(conditions['Sun hours'][2])
+
+                   \
+    .groupby(['City', 'Measure'])['Value'].mean()
